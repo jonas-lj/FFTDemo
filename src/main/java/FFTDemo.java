e0d0e110714ec0b97b24e91fcbb1d56a87f3b8be
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-05-11 10:05:18 +0200

    Update

--- src/main/java/FFTDemo.java
+++ src/main/java/FFTDemo.java
@@ -14,6 +14,7 @@ import dk.alexandra.fresco.framework.sce.SecureComputationEngine;
 import dk.alexandra.fresco.framework.sce.SecureComputationEngineImpl;
 import dk.alexandra.fresco.framework.sce.evaluator.BatchEvaluationStrategy;
 import dk.alexandra.fresco.framework.sce.evaluator.BatchedProtocolEvaluator;
+import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
 import dk.alexandra.fresco.framework.util.AesCtrDrbg;
 import dk.alexandra.fresco.lib.field.integer.BasicNumericContext;
 import dk.alexandra.fresco.lib.fixed.AdvancedFixedNumeric;
@@ -23,11 +24,15 @@ import dk.alexandra.fresco.stat.complex.OpenComplex;
 import dk.alexandra.fresco.stat.complex.SecretComplex;
 import dk.alexandra.fresco.stat.linearalgebra.FFT;
 import dk.alexandra.fresco.suite.crt.CRTProtocolSuite;
-import dk.alexandra.fresco.suite.crt.datatypes.resource.*;
+import dk.alexandra.fresco.suite.crt.datatypes.resource.CRTDataSupplier;
+import dk.alexandra.fresco.suite.crt.datatypes.resource.CRTResourcePool;
+import dk.alexandra.fresco.suite.crt.datatypes.resource.CRTResourcePoolImpl;
+import dk.alexandra.fresco.suite.crt.datatypes.resource.CRTSemiHonestDataSupplier;
 import dk.alexandra.fresco.suite.crt.fixed.CRTAdvancedFixedNumeric;
 import dk.alexandra.fresco.suite.crt.fixed.CRTFixedNumeric;
 import dk.alexandra.fresco.suite.crt.protocols.framework.CRTSequentialStrategy;
 import dk.alexandra.fresco.suite.spdz.SpdzBuilder;
+import dk.alexandra.fresco.suite.spdz.SpdzProtocolSuite;
 import dk.alexandra.fresco.suite.spdz.SpdzResourcePool;
 import dk.alexandra.fresco.suite.spdz.SpdzResourcePoolImpl;
 import dk.alexandra.fresco.suite.spdz.storage.SpdzDataSupplier;
@@ -46,118 +51,133 @@ import java.util.stream.Collectors;
 
 public class FFTDemo {
 
-  protected static final FieldDefinition DEFAULT_FIELD_LEFT =
-          MersennePrimeFieldDefinition.find(64);
-  protected static final FieldDefinition DEFAULT_FIELD_RIGHT = new BigIntegerFieldDefinition(
-          new BigInteger(152 + 40, new Random(1234)).nextProbablePrime());
-  
-  protected static final FieldDefinition DEFAULT_FIELD = new BigIntegerFieldDefinition(
-          new BigInteger(256, new Random(1234)).nextProbablePrime());
+    protected static final FieldDefinition DEFAULT_FIELD_LEFT =
+            MersennePrimeFieldDefinition.find(64);
+    protected static final FieldDefinition DEFAULT_FIELD_RIGHT = new BigIntegerFieldDefinition(
+            new BigInteger(192, new Random(1234)).nextProbablePrime());
+    protected static final FieldDefinition DEFAULT_FIELD = new BigIntegerFieldDefinition(
+            new BigInteger(256, new Random(1234)).nextProbablePrime());
 
-  public static void main(String[] arguments) throws IOException {
-    if (arguments.length != 2) {
-      throw new IllegalArgumentException("Usage: java Demo [myId] [otherIP]");
-    }
-
-    // Configure fresco
-    final int myId = Integer.parseInt(arguments[0]);
-    final String otherIP = arguments[1];
-    final int noParties = 2;
-    final int otherId = 3 - myId;
-    final int modBitLength = 256;
-    final int maxBitLength = 180;
-    final int maxBatchSize = 4096;
-
-    Party me = new Party(myId, "localhost", 9000 + myId);
-    Party other = new Party(otherId, otherIP, 9000 + otherId);
-    NetworkConfiguration networkConfiguration = new NetworkConfigurationImpl(myId,
-        Map.of(myId, me, otherId, other));
-    Network network = new SocketNetwork(networkConfiguration);
-
-//    SpdzProtocolSuite suite = new SpdzProtocolSuite(maxBitLength);
-//
-//    // Use "dummy" multiplication triples to simulate doing only the online phase
-//    SpdzDataSupplier supplier = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD,
-//            BigInteger.valueOf(1234));
-//
-//    SpdzResourcePool rp = new SpdzResourcePoolImpl(myId, noParties,
-//            new SpdzOpenedValueStoreImpl(), supplier,
-//            AesCtrDrbg::new);
-//
-//    BatchedProtocolEvaluator<SpdzResourcePool> evaluator =
-//            new BatchedProtocolEvaluator<>(EvaluationStrategy.SEQUENTIAL_BATCHED.getStrategy(), suite,
-//                    maxBatchSize);
-//    SecureComputationEngine<SpdzResourcePool, ProtocolBuilderNumeric> sce = new SecureComputationEngineImpl<>(
-//            suite, evaluator);
-
-    BatchEvaluationStrategy<CRTResourcePool<SpdzResourcePool, SpdzResourcePool>> batchEvaluationStrategy =
-            new CRTSequentialStrategy<>();
-
-        SpdzDataSupplier supplierLeft = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD_LEFT,
-            BigInteger.valueOf(1234));
-    SpdzResourcePool rpLeft = new SpdzResourcePoolImpl(myId, noParties,
-            new SpdzOpenedValueStoreImpl(), supplierLeft,
-            AesCtrDrbg::new);
-    SpdzDataSupplier supplierRight = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD_RIGHT,
-            BigInteger.valueOf(1234));
-    SpdzResourcePool rpRight = new SpdzResourcePoolImpl(myId, noParties,
-            new SpdzOpenedValueStoreImpl(), supplierRight,
-            AesCtrDrbg::new);
-    CRTDataSupplier<SpdzResourcePool, SpdzResourcePool> dataSupplier = new CRTCovertDataSupplier<>(null);
-
-    CRTResourcePool<SpdzResourcePool, SpdzResourcePool> rp =
-            new CRTResourcePoolImpl<>(myId, noParties, dataSupplier, rpLeft, rpRight);
-
-    CRTProtocolSuite<SpdzResourcePool, SpdzResourcePool> ps =
-            new CRTProtocolSuite<>(
-                    new SpdzBuilder(new BasicNumericContext(DEFAULT_FIELD_LEFT.getBitLength(),
-                            myId, noParties, DEFAULT_FIELD_LEFT, 16, 40)),
-                    new SpdzBuilder(new BasicNumericContext(DEFAULT_FIELD_RIGHT.getBitLength(),
-                            myId, noParties, DEFAULT_FIELD_RIGHT, 16, 40)));
-    ProtocolEvaluator<CRTResourcePool<SpdzResourcePool,
-            SpdzResourcePool>> evaluator =
-            new BatchedProtocolEvaluator<>(batchEvaluationStrategy, ps);
-
-    SecureComputationEngine<CRTResourcePool<SpdzResourcePool,
-            SpdzResourcePool>, ProtocolBuilderNumeric> sce =
-            new SecureComputationEngineImpl<>(ps, evaluator);
-
-    System.out.println("Loading CRT fixed numeric...");
-    FixedNumeric.load(CRTFixedNumeric::new);
-    AdvancedFixedNumeric.load(CRTAdvancedFixedNumeric::new);
-
-    Instant start = Instant.now();
-
-    List<OpenComplex> out = sce
-        .runApplication(new FFTApp(2048),
-            rp, network, Duration.ofMinutes(30));
-
-    System.out.println(out);
-    System.out.println("Took " + Duration.between(start, Instant.now()));
-  }
-
-  public static class FFTApp implements
-      Application<List<OpenComplex>, ProtocolBuilderNumeric> {
-
-    private final int n;
-
-    public FFTApp(int n) {
-      this.n = n;
-    }
-
-    @Override
-    public DRes<List<OpenComplex>> buildComputation(ProtocolBuilderNumeric builder) {
-      return builder.par(par -> {
-        ArrayList<DRes<SecretComplex>> inputs = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-          inputs.add(new SecretComplex(FixedNumeric.using(par).known(i), FixedNumeric.using(par).known(0)));
+    public static void main(String[] arguments) throws IOException {
+        if (arguments.length != 4) {
+            throw new IllegalArgumentException("Usage: java Demo [myId] [otherIP] [n] [CRT/SPDZ]");
         }
-        return DRes.of(inputs);
-      }).seq((seq, inputs) -> new FFT(inputs).buildComputation(seq)).
-      par((par, result) ->
-        DRes.of(result.stream().map(z -> new Open(z.out()).buildComputation(par)).collect(Collectors.toList()))
-      ).par((par, result) -> DRes.of(result.stream().map(DRes::out).collect(Collectors.toList()))
-    );
+
+        // Configure fresco
+        final int myId = Integer.parseInt(arguments[0]);
+        final String otherIP = arguments[1];
+        final int noParties = 2;
+        final int otherId = 3 - myId;
+        int n = Integer.parseInt(arguments[2]);
+
+        final int modBitLength = 256;
+        final int maxBitLength = 180;
+        final int maxBatchSize = 4096;
+
+        Party me = new Party(myId, "localhost", 9000 + myId);
+        Party other = new Party(otherId, otherIP, 9000 + otherId);
+        NetworkConfiguration networkConfiguration = new NetworkConfigurationImpl(myId,
+                Map.of(myId, me, otherId, other));
+        Network network = new SocketNetwork(networkConfiguration);
+
+
+        String scheme = arguments[3];
+        Instant start;
+        List<OpenComplex> results;
+
+        if (scheme.equalsIgnoreCase("crt")) {
+            BatchEvaluationStrategy<CRTResourcePool<SpdzResourcePool, SpdzResourcePool>> batchEvaluationStrategy =
+                    new CRTSequentialStrategy<>();
+
+            SpdzDataSupplier supplierLeft = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD_LEFT,
+                    BigInteger.valueOf(1234));
+            SpdzResourcePool rpLeft = new SpdzResourcePoolImpl(myId, noParties,
+                    new SpdzOpenedValueStoreImpl(), supplierLeft,
+                    AesCtrDrbg::new);
+            SpdzDataSupplier supplierRight = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD_RIGHT,
+                    BigInteger.valueOf(1234));
+            SpdzResourcePool rpRight = new SpdzResourcePoolImpl(myId, noParties,
+                    new SpdzOpenedValueStoreImpl(), supplierRight,
+                    AesCtrDrbg::new);
+            CRTDataSupplier<SpdzResourcePool, SpdzResourcePool> dataSupplier = new CRTSemiHonestDataSupplier<>(null);
+
+            CRTResourcePool<SpdzResourcePool, SpdzResourcePool> rp =
+                    new CRTResourcePoolImpl<>(myId, noParties, dataSupplier, rpLeft, rpRight);
+
+            CRTProtocolSuite<SpdzResourcePool, SpdzResourcePool> ps =
+                    new CRTProtocolSuite<>(
+                            new SpdzBuilder(new BasicNumericContext(DEFAULT_FIELD_LEFT.getBitLength(),
+                                    myId, noParties, DEFAULT_FIELD_LEFT, 16, 40)),
+                            new SpdzBuilder(new BasicNumericContext(DEFAULT_FIELD_RIGHT.getBitLength(),
+                                    myId, noParties, DEFAULT_FIELD_RIGHT, 16, 40)));
+            ProtocolEvaluator<CRTResourcePool<SpdzResourcePool,
+                    SpdzResourcePool>> evaluator =
+                    new BatchedProtocolEvaluator<>(batchEvaluationStrategy, ps);
+
+            SecureComputationEngine<CRTResourcePool<SpdzResourcePool,
+                    SpdzResourcePool>, ProtocolBuilderNumeric> sce =
+                    new SecureComputationEngineImpl<>(ps, evaluator);
+
+            System.out.println("Loading CRT fixed numeric...");
+            FixedNumeric.load(CRTFixedNumeric::new);
+            AdvancedFixedNumeric.load(CRTAdvancedFixedNumeric::new);
+
+            start = Instant.now();
+
+            results = sce
+                    .runApplication(new FFTApp(n),
+                            rp, network, Duration.ofMinutes(30));
+
+        } else if (scheme.equalsIgnoreCase("spdz")) {
+            SpdzProtocolSuite suite = new SpdzProtocolSuite(maxBitLength);
+
+            // Use "dummy" multiplication triples to simulate doing only the online phase
+            SpdzDataSupplier supplier = new SpdzDummyDataSupplier(myId, noParties, DEFAULT_FIELD,
+                    BigInteger.valueOf(1234));
+
+            SpdzResourcePool rp = new SpdzResourcePoolImpl(myId, noParties,
+                    new SpdzOpenedValueStoreImpl(), supplier,
+                    AesCtrDrbg::new);
+
+            BatchedProtocolEvaluator<SpdzResourcePool> evaluator =
+                    new BatchedProtocolEvaluator<>(EvaluationStrategy.SEQUENTIAL.getStrategy(), suite,
+                            maxBatchSize);
+            SecureComputationEngine<SpdzResourcePool, ProtocolBuilderNumeric> sce = new SecureComputationEngineImpl<>(
+                    suite, evaluator);
+
+            start = Instant.now();
+            results = sce
+                    .runApplication(new FFTApp(n),
+                            rp, network, Duration.ofMinutes(30));
+        } else {
+            throw new IllegalArgumentException("Unknown scheme: " + scheme);
+        }
+
+        System.out.println(results);
+        System.out.println("Took " + Duration.between(start, Instant.now()));
     }
-  }
+
+    /** Compute the fast fourier transform of <i>(0,...,0) &isin; &#8450;<sup>n</sup></i>. */
+    public static class FFTApp implements
+            Application<List<OpenComplex>, ProtocolBuilderNumeric> {
+
+        private final int n;
+
+        public FFTApp(int n) {
+            this.n = n;
+        }
+
+        @Override
+        public DRes<List<OpenComplex>> buildComputation(ProtocolBuilderNumeric builder) {
+            return builder.par(par -> {
+                ArrayList<DRes<SecretComplex>> inputs = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    inputs.add(new SecretComplex(FixedNumeric.using(par).known(i), FixedNumeric.using(par).known(0)));
+                }
+                return DRes.of(inputs);
+            }).seq((seq, inputs) -> new FFT(inputs).buildComputation(seq)).par((par, result) ->
+                    DRes.of(result.stream().map(z -> new Open(z.out()).buildComputation(par)).collect(Collectors.toList()))
+            ).par((par, result) -> DRes.of(result.stream().map(DRes::out).collect(Collectors.toList())));
+        }
+    }
 }
