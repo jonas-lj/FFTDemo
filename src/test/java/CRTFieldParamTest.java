e7773ecdde5dfbe7d518f1115437b6521d416030
Tore Frederiksen <jot2re@gmail.com>
2023-09-26 11:23:08 +0200

    test test of constraints

--- src/test/java/CRTFieldParamTest.java
+++ src/test/java/CRTFieldParamTest.java
@@ -1,6 +1,8 @@
 import dk.alexandra.fresco.framework.builder.numeric.field.BigIntegerFieldDefinition;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,6 +14,7 @@ public class CRTFieldParamTest {
         assertEquals(64, params.getP().getModulus().bitLength());
         assertEquals(128, params.getQ().getModulus().bitLength());
         assertEquals(192, params.getMaxAllowedValue().bitLength());
+        assertEquals(BigInteger.ZERO, params.getQ().getModulus().subtract(BigInteger.ONE).shiftRight(1).mod(params.getP().getModulus()));
         assertTrue(params.getP().getModulus().isProbablePrime(40));
         assertTrue(params.getQ().getModulus().isProbablePrime(40));
 
@@ -19,6 +22,7 @@ public class CRTFieldParamTest {
         assertEquals(40, params.getP().getModulus().bitLength());
         assertEquals(88, params.getQ().getModulus().bitLength());
         assertEquals(128, params.getMaxAllowedValue().bitLength());
+        assertEquals(BigInteger.ZERO, params.getQ().getModulus().subtract(BigInteger.ONE).shiftRight(1).mod(params.getP().getModulus()));
         assertTrue(params.getP().getModulus().isProbablePrime(40));
         assertTrue(params.getQ().getModulus().isProbablePrime(40));
 
@@ -27,6 +31,7 @@ public class CRTFieldParamTest {
         // Notice the rounding of parameters mean we can get something a bit larger
         assertEquals(152, params.getQ().getModulus().bitLength());
         assertEquals(256, params.getMaxAllowedValue().bitLength());
+        assertEquals(BigInteger.ZERO, params.getQ().getModulus().subtract(BigInteger.ONE).shiftRight(1).mod(params.getP().getModulus()));
         assertTrue(params.getP().getModulus().isProbablePrime(40));
         assertTrue(params.getQ().getModulus().isProbablePrime(40));
 
@@ -35,6 +40,7 @@ public class CRTFieldParamTest {
         assertEquals(288, params.getQ().getModulus().bitLength());
         // Notice the rounding of parameters mean we can get something a bit larger
         assertEquals(520, params.getMaxAllowedValue().bitLength());
+        assertEquals(BigInteger.ZERO, params.getQ().getModulus().subtract(BigInteger.ONE).shiftRight(1).mod(params.getP().getModulus()));
         assertTrue(params.getP().getModulus().isProbablePrime(40));
         assertTrue(params.getQ().getModulus().isProbablePrime(40));
 
@@ -42,6 +48,7 @@ public class CRTFieldParamTest {
         assertEquals(216, params.getP().getModulus().bitLength());
         assertEquals(296, params.getQ().getModulus().bitLength());
         assertEquals(512, params.getMaxAllowedValue().bitLength());
+        assertEquals(BigInteger.ZERO, params.getQ().getModulus().subtract(BigInteger.ONE).shiftRight(1).mod(params.getP().getModulus()));
         assertTrue(params.getP().getModulus().isProbablePrime(40));
         assertTrue(params.getQ().getModulus().isProbablePrime(40));
     }
